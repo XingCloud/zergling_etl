63c2bdd67ef792d766916907e0a914645c1f18a2
labelnolongerexists <labelnolongerexists@gmail.com>
2014-02-25 14:37:00 +0800

    1.0 commit

--- bin/run_zergling_etl.sh
+++ bin/run_zergling_etl.sh
@@ -13,19 +13,24 @@ else
   echo "User defined processing day found."
   processing_day=$1
 fi
+processing_history_day=`date -u -d"${processing_day} 7 days ago" +%Y-%m-%d`
 raw_log_web=http://log.goo.mx/clicklogs/log.goo.mx.access-${processing_day}.tar.gz
 raw_log_path=/data/raw/22find/nav/
 default_unpack_name=home/elex/serversoft/nginx/log/log.goo.mx.access.log
 project_id=22find
 hdfs_history_path=/user/hadoop/history/raw/22find/nav
+current_op_file_name=${project_id}.processing_day
+history_op_file_name=${project_id}.processing_history_day
+
 echo ${line}
 echo "[PROCESSING-DAY]="${processing_day}
+echo "[PROCESSING-HISTORY-DAY]="${processing_history_day}
 echo "[PROJECT-ID]="${project_id}
 echo "[HTTP-RAW-LOG]="${raw_log_web}
 echo "[DOWNLOAD-OUTPUT]="${raw_log_path}
 echo ${line}
 
-wget ${raw_log_web} -O /data/raw/22find/nav/${project_id}.${processing_day}.tar.gz
+wget ${raw_log_web} -O ${raw_log_path}/${current_op_file_name}.tar.gz
 if [ $? -ne 0 ];then
   echo "Downloading from http://log.goo.mx/clicklogs is failed."
   exit 1
@@ -34,8 +39,8 @@ else
 fi
 echo ${line}
 echo "Unpacking and flattening path."
-tar zxvf ${raw_log_path}/22find.2014-02-24.tar.gz -C ${raw_log_path}
-mv  ${raw_log_path}/home/elex/serversoft/nginx/log/log.goo.mx.access.log ${raw_log_path}/22find.${processing_day}.log
+tar zxvf ${raw_log_path}/${current_op_file_name}.tar.gz -C ${raw_log_path}
+mv  ${raw_log_path}/home/elex/serversoft/nginx/log/log.goo.mx.access.log ${raw_log_path}/${current_op_file_name}.log
 rm -rf ${raw_log_path}/home
 echo ${line}
 echo "Copy local to hdfs backup."
@@ -44,5 +49,15 @@ if [ $? -ne 0 ];then
   echo "Backup path does not exist. Create it now(${hdfs_history_path})."
   ${hadoop_home}/hadoop fs -mkdir ${hdfs_history_path}
 fi
+${hadoop_home}/hadoop fs -copyFromLocal ${raw_log_path}/${current_op_file_name}.tar.gz ${hdfs_history_path}
 echo ${line}
+
+${hadoop_home}/hadoop fs -test -e ${hdfs_history_path}/${history_op_file_name}.tar.gz
+if [ $? -ne 0 ];then
+  echo "Old raw file does not exist on hdfs(${hdfs_history_path}/${history_op_file_name}.tar.gz), skip to remove local old file."
+else
+  echo "Remove old file from local(${hdfs_history_path}/${history_op_file_name}.tar.gz)."
+  rm -rf ${raw_log_path}/${history_op_file_name}.tar.gz
+fi
+
 #mvn -f D:/git_home_new/zergling_etl/pom.xml exec:java -Dexec.mainClass="com.elex.bigdata.zergling.etl.NavigatorETL" -Dexec.args="2014-02-24 22find D:/22find/log.goo.mx.access.log D:/22find/22find.nav.log" -Dexec.classpathScope=runtime
