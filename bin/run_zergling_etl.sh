093771c49595214cf920963de9d3dd65720010ee
labelnolongerexists <labelnolongerexists@gmail.com>
2014-02-25 14:16:33 +0800

    1.0 commit

--- bin/run_zergling_etl.sh
+++ bin/run_zergling_etl.sh
@@ -3,6 +3,7 @@
 line="############################################"
 # Code base
 jar_home=/home/hadoop/git_project_home/zergling_etl/target/zergling_etl.jar
+hadoop_home=/usr/bin/
 
 echo ${line}
 if [ "" = "$1" ];then
@@ -16,6 +17,7 @@ raw_log_web=http://log.goo.mx/clicklogs/log.goo.mx.access-${processing_day}.tar.
 raw_log_path=/data/raw/22find/nav/
 default_unpack_name=home/elex/serversoft/nginx/log/log.goo.mx.access.log
 project_id=22find
+hdfs_history_path=/user/hadoop/history/raw/22find/nav
 echo ${line}
 echo "[PROCESSING-DAY]="${processing_day}
 echo "[PROJECT-ID]="${project_id}
@@ -36,5 +38,11 @@ tar zxvf ${raw_log_path}/22find.2014-02-24.tar.gz -C ${raw_log_path}
 mv  ${raw_log_path}/home/elex/serversoft/nginx/log/log.goo.mx.access.log ${raw_log_path}/22find.${processing_day}.log
 rm -rf ${raw_log_path}/home
 echo ${line}
-
+echo "Copy local to hdfs backup."
+${hadoop_home}/hadoop fs -test -d ${hdfs_history_path}
+if [ $? -ne 0 ];then
+  echo "Backup path does not exist. Create it now(${hdfs_history_path})."
+  ${hadoop_home}/hadoop fs -mkdir ${hdfs_history_path}
+fi
+echo ${line}
 #mvn -f D:/git_home_new/zergling_etl/pom.xml exec:java -Dexec.mainClass="com.elex.bigdata.zergling.etl.NavigatorETL" -Dexec.args="2014-02-24 22find D:/22find/log.goo.mx.access.log D:/22find/22find.nav.log" -Dexec.classpathScope=runtime
