b195aab4ea62815ca2c555535747637eb693be51
labelnolongerexists <labelnolongerexists@gmail.com>
2014-03-03 15:52:01 +0800

    Add hbase error handling.

--- src/main/java/com/elex/bigdata/zergling/etl/hbase/HBasePutter.java
+++ src/main/java/com/elex/bigdata/zergling/etl/hbase/HBasePutter.java
@@ -83,8 +83,8 @@ public class HBasePutter implements Runnable {
           e.printStackTrace();
         } finally {
           long t2 = System.currentTimeMillis();
-          LOGGER.info("Status=" + (successful ? "ok" : "with-err"
-          ) + "CostInMilliseconds(" + Thread.currentThread().getName() + ")=" + (t2 - t1));
+          LOGGER.info("Status=[" + (successful ? "ok" : "with-err"
+          ) + "], CostInMilliseconds(" + Thread.currentThread().getName() + ")=" + (t2 - t1));
         }
       }
     } catch (InterruptedException e) {
