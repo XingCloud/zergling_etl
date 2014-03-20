96109eef63678d5461ca028f662e0d822efc4224
griffinl <lq0987@126.com>
2014-03-20 16:34:26 +0800

    调整脚本

--- bin/build_zergling_etl.sh
+++ bin/build_zergling_etl.sh
@@ -39,5 +39,5 @@ fi
 echo "[CHECK-POINT] - Packaging."
 #mvn -f ${code_home}/pom.xml clean package -DskipTests=true -Daid=${aid} -Denv=${env}
 #mvn -f ${code_home}/pom.xml clean compile assembly:single -DskipTests=true -Daid=${aid} -Denv=${env}
-mvn clean package -DskipTests
+mvn -f ${code_home}/pom.xml clean package -DskipTests
 echo "[CHECK-POINT] - Packaging is done."
