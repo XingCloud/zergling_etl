6ce492e1c16c0415cc52e0821155772f6ce24fcd
griffinl <lq0987@126.com>
2014-03-20 16:26:57 +0800

    调整脚本

---
+++ bin/dayrun.22find.search.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+if [ $# -eq 0 ];then
+   day=`date -d "2 days ago" +%Y%m%d`
+else
+   day=$1
+fi
+
+daily_log_path="/data/bigdata/22find/search/search_${day}.log"
+sh log_import.sh 22find search_22find search ${day} >> ${daily_log_path}
+if grep -Fxq "without error" ${daily_log_path}
+then
+    echo "No Exception"
+else
+    echo "Found Exception"
+fi
