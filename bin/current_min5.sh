dc724a9e13b7bb4e7db0fcc4bf264d82ba439201
labelnolongerexists <labelnolongerexists@gmail.com>
2014-03-31 16:31:26 +0800

    New version navigator log ETL for all project.

--- bin/current_min5.sh
+++ bin/current_min5.sh
@@ -4,4 +4,5 @@ code_home=/home/hadoop/git_project_home/zergling_etl
 jar_home=${code_home}/target/zergling_etl.CurrentMin5.jar-with-dependencies.jar
 java_bin=/usr/java/jdk1.7.0_45/bin
 
-processing_history_day=`${java_bin}/java -jar ${jar_home}`
+current_min5=`${java_bin}/java -jar ${jar_home}`
+echo ${current_min5}
