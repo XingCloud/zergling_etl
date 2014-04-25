05b533bdc96f2a7508ef7ae4dadf7376efcea37f
labelnolongerexists <labelnolongerexists@gmail.com>
2014-04-25 10:51:53 +0800

    Error handle

--- bin/generic_cleaner.sh
+++ bin/generic_cleaner.sh
@@ -12,41 +12,46 @@ else
   processing_date=$1
 fi
 
-type=nav
-input_path=${source_file_home}/${type}/${processing_date}
+types="nav nt pc ps"
 
-if [ ! -d ${input_path} ];then
-  echo "Input date(${processing_date}) does not exist."
-  exit 1
-fi
+for type in ${types};do
+  echo "======Current type: ${type}========"
+  input_path=${source_file_home}/${type}/${processing_date}
 
-merged_and_local_history_file=${local_history_home}/${type}/${type}.${processing_date}.log
-echo "Merging(Type=${type}, date=${processing_date})"
-cat ${input_path}/* >> ${merged_and_local_history_file}
+  if [ ! -d ${input_path} ];then
+    echo "Input date(${processing_date}) does not exist."
+    exit 1
+  fi
 
-compressed_local_history_file=${merged_and_local_history_file}.tar.gz
-echo "Compressing(Type=${type}, date=${processing_date})"
-tar zcvf ${compressed_local_history_file} ${merged_and_local_history_file}
+  merged_and_local_history_file=${local_history_home}/${type}/${type}.${processing_date}.log
+  echo "Merging(Type=${type}, date=${processing_date})"
+  cat ${input_path}/* >> ${merged_and_local_history_file}
 
-echo "Backuping(Type=${type}, date=${processing_date})"
-history_file=${hdfs_history_home}/${type}/${type}.${processing_date}.log.tar.gz
-${hadoop_home}/hadoop fs -test -e ${history_file}
-if [ $? -ne 0 ];then
-  echo "Copy directly(${history_file})."
-else
-  echo "Remove exists history file.(${history_file})."
-  ${hadoop_home}/hadoop fs -rm -r ${history_file}
-fi
-${hadoop_home}/hadoop fs -copyFromLocal ${compressed_local_history_file} ${history_file}
+  compressed_local_history_file=${merged_and_local_history_file}.tar.gz
+  echo "Compressing(Type=${type}, date=${processing_date})"
+  tar zcvf ${compressed_local_history_file} ${merged_and_local_history_file}
 
-history_processing_date=`date -d"${processing_date} -7 days" +%Y%m%d`
-too_old_history_path1=${source_file_home}/${type}/${history_processing_date}
-too_old_history_path2=${local_history_home}/${type}/${type}.${history_processing_date}.log
-too_old_history_path3=${local_history_home}/${type}/${type}.${history_processing_date}.log.tar.gz
-echo "Cleaning(${too_old_history_path1})"
-rm -rf ${too_old_history_path1}
-echo "Cleaning(${too_old_history_path2})"
-rm -rf ${too_old_history_path2}
-echo "Cleaning(${too_old_history_path3})"
-rm -rf ${too_old_history_path3}
+  echo "Backuping(Type=${type}, date=${processing_date})"
+  history_file=${hdfs_history_home}/${type}/${type}.${processing_date}.log.tar.gz
+  ${hadoop_home}/hadoop fs -test -e ${history_file}
+  if [ $? -ne 0 ];then
+    echo "Copy directly(${history_file})."
+  else
+    echo "Remove exists history file.(${history_file})."
+    ${hadoop_home}/hadoop fs -rm -r ${history_file}
+  fi
+  ${hadoop_home}/hadoop fs -copyFromLocal ${compressed_local_history_file} ${history_file}
+
+  history_processing_date=`date -d"${processing_date} -7 days" +%Y%m%d`
+  too_old_history_path1=${source_file_home}/${type}/${history_processing_date}
+  too_old_history_path2=${local_history_home}/${type}/${type}.${history_processing_date}.log
+  too_old_history_path3=${local_history_home}/${type}/${type}.${history_processing_date}.log.tar.gz
+  echo "Cleaning(${too_old_history_path1})"
+  rm -rf ${too_old_history_path1}
+  echo "Cleaning(${too_old_history_path2})"
+  rm -rf ${too_old_history_path2}
+  echo "Cleaning(${too_old_history_path3})"
+  rm -rf ${too_old_history_path3}
+done
+
 echo "All done"
