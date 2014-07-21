92ed0005ff7036f2f21edb86a88271b44e638db3
griffinl <lq0987@126.com>
2014-07-21 18:24:50 +0800

    update

--- bin/log_import.sh
+++ bin/log_import.sh
@@ -39,31 +39,7 @@ echo "[FILE-NAME]="${current_op_file_name}
 echo ${line}
 
 
-echo "Copy local to hdfs backup."
-
-${hadoop_home}/hadoop fs -test -d ${hdfs_history_path}
-if [ $? -ne 0 ];then
-  echo "Backup path does not exist. Create it now(${hdfs_history_path})."
-  ${hadoop_home}/hadoop fs -mkdir ${hdfs_history_path}
-fi
-
-${hadoop_home}/hadoop fs -test -e ${hdfs_history_path}/${current_op_file_name}
-if [ $? -ne 0 ];then
-  echo "Copy directly(${hdfs_history_path}/${current_op_file_name})."
-else
-  echo "Remove exists history file.(${hdfs_history_path}/${current_op_file_name})."
-  ${hadoop_home}/hadoop fs -rm -r  ${hdfs_history_path}/${current_op_file_name}
-fi
-${hadoop_home}/hadoop fs -copyFromLocal ${raw_log_path}/${current_op_file_name} ${hdfs_history_path}
-echo ${line}
-
-${hadoop_home}/hadoop fs -test -e ${hdfs_history_path}/${history_op_file_name}
-if [ $? -ne 0 ];then
-  echo "Old raw file does not exist on hdfs(${hdfs_history_path}/${history_op_file_name}), skip to remove local old file."
-else
-  echo "Remove old file from local(${hdfs_history_path}/${history_op_file_name})."
-  rm -rf ${raw_log_path}/${history_op_file_name}
-fi
+#echo "Copy local to hdfs backup."
 
 ${java_bin}/java  -jar ${jar_home} ${type} ${table_name} ${raw_log_path}/${current_op_file_name} ${workers} ${batch_size} ${project_id}
 echo "All done"
