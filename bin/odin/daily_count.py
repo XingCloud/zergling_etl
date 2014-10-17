7f49fb440dc27da6573938b25ca18acf6b58b79b
griffinl <lq0987@126.com>
2014-10-17 13:37:44 +0800

    update

--- bin/odin/daily_count.py
+++ bin/odin/daily_count.py
@@ -16,7 +16,8 @@ insert overwrite local directory '/data1/odin/dayily_count'
 row format delimited
 fields terminated by ','
 select * from (
-select '','nav visit','visit reqid','visit uid','search','search reqid','search uid','ad imp','imp reqid','imp uid'
+select '','nav visit','visit reqid','visit uid','search','search reqid','search uid','ad imp','imp reqid','imp uid' from dual
+union all
 select visit.pid,visit.pr,visit.pv,visit.pu,se.sv,se.sr,se.su,'','','' from
 (select pid, count(*) pv , count(distinct reqid) pr, count(distinct uid) pu from odin.nav_visit where day='%s' group by pid ) visit join
 (select pid, count(*) sv, count(distinct reqid) sr, count(distinct uid) su from odin.search where day='%s' group by pid) se on visit.pid = se.pid
