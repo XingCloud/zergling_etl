c00b31fb30ed904ee3e83b1051fe093e82429bc1
griffinl <lq0987@126.com>
2014-10-23 17:17:53 +0800

    update

--- bin/odin/daily_partition.sh
+++ bin/odin/daily_partition.sh
@@ -5,7 +5,7 @@ else
     day=`date +%Y%m%d`
 fi
 
-for project in nv search ad_imp gdp
+for project in nv search ad_imp gdp ac
 do
     hadoop fs -mkdir odin/$project/$day
 done
@@ -14,6 +14,7 @@ hive -e " use odin;
     alter table nav_visit add partition(day='$day') location '/user/hadoop/odin/nv/$day/' ;
     alter table search add partition(day='$day') location '/user/hadoop/odin/search/$day/' ;
     alter table ad_impression add partition(day='$day') location '/user/hadoop/odin/ad_imp/$day/';
-    alter table gdp add partition(day='$day') location '/user/hadoop/odin/gdp/$day/'; "
+    alter table gdp add partition(day='$day') location '/user/hadoop/odin/gdp/$day/';
+    alter table ad_click add partition(day='$day') location '/user/hadoop/odin/ac/$day/'; "
 
 
