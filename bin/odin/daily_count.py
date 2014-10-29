1d48aa5a0dfaabad73607bce708725cd5362a470
griffinl <lq0987@126.com>
2014-10-29 13:09:56 +0800

    update

--- bin/odin/daily_count.py
+++ bin/odin/daily_count.py
@@ -20,13 +20,13 @@ select visit.pid,visit.pv,visit.pr,visit.pu,se.sv,se.sr,se.su,an.iv,an.ir,an.iu,
 (select pid, count(*) pv, count(distinct reqid) pr, count(distinct uid) pu from odin.nav_visit where day='%s' group by pid ) visit join
 (select pid, count(*) sv, count(distinct reqid) sr, count(distinct uid) su from odin.search where day='%s' group by pid) se on visit.pid = se.pid left outer join
 (select pid, count(*) iv, count(distinct reqid) ir, count(distinct uid) iu from odin.ad_impression where day='%s' group by pid) an on an.pid = se.pid left outer join
-(select pid, count(*) cv,count(distinct ai.reqid) cr, count(distinct ai.uid) cu from  ad_click ac  join ad_impression ai on ai.reqid = ac.reqid and ai.day = ac.day  where ac.day = '%s' group by pid) ac on ac.pid = se.pid
+(select pid, count(*) cv, count(distinct ai.reqid) cr, count(distinct ai.uid) cu from ad_click ac join ad_impression ai on ai.reqid = ac.reqid and ai.day = ac.day where ac.day = '%s' group by pid) ic on ic.pid = visit.pid
 union all
 select tv.pid,tv.pv,tv.pr,tv.pu,ts.sv,ts.sr,ts.su,ti.iv,ti.ir,ti.iu,tc.cv,tc.cr,tc.cu from
 (select 'total' pid, count(*) pv , count(distinct reqid) pr,count(distinct uid) pu from odin.nav_visit where day='%s') tv join
 (select 'total' pid, count(*) sv, count(distinct reqid) sr,count(distinct uid) su from odin.search where day='%s') ts on tv.pid = ts.pid join
 (select 'total' pid, count(*) iv, count(distinct reqid) ir, count(distinct uid) iu from odin.ad_impression where day='%s') ti on ti.pid = ts.pid join
-(select 'total' pid, count(*) cv, count(distinct reqid) cr, '' cu from odin.ad_click where day='%s') tc on tc.pid = ts.pid ) tmp
+(select 'total' pid, count(*) cv, count(distinct reqid) cr, '' cu from odin.ad_click where day='%s') tc on tc.pid = ti.pid ) tmp
 '''
 
 def sendMail(subject,content, filename):
@@ -67,7 +67,7 @@ def attach_file(filename):
     return file_msg
 
 def count_odin(day):
-    daily_sql = sql%(day, day, day, day, day, day,day,day)
+    daily_sql = sql%(day, day, day, day, day, day, day, day)
     print 'rm -rf /data1/odin/dayily_count/'
     os.system('rm -rf /data1/odin/dayily_count/')
     print "hive -e \"%s\"" % daily_sql
