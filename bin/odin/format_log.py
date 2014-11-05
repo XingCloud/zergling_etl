a71c48f8b2c437f74aadccdb205ab1bf8faa56b0
griffinl <lq0987@126.com>
2014-11-05 09:43:18 +0800

    update

--- bin/odin/format_log.py
+++ bin/odin/format_log.py
@@ -141,7 +141,7 @@ def parse_nv_line(line):
         ip = attrs[0].split(", ")[0]
         #p time reqid uid ip nation ua os width height refer
         return "%s\t%s %s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s"%(pid, time[:10],time[11:],params["reqID"],params["User_id"],ip,
-                nation,ua,params["os"],params["Screen_width"],params["Screen_Height"],refer)
+                nation,ua,params["os"],params["Screen_width"],params["Screen_Height"],'')
 
     except Exception,e:
         print e
