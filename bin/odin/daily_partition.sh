00aae8f558a4d1c93a6f3810920fb09381b52bd1
griffinl <lq0987@126.com>
2014-12-29 13:19:24 +0800

    update

--- bin/odin/daily_partition.sh
+++ bin/odin/daily_partition.sh
@@ -14,7 +14,7 @@ hive -e " use odin;
     alter table search add partition(day='$day') location '/user/hadoop/odin/search/$day/' ;
     alter table ad_impression add partition(day='$day') location '/user/hadoop/odin/ad_imp/$day/';
     alter table gdp add partition(day='$day') location '/user/hadoop/odin/gdp/$day/';
-    alter table ad_click add partition(day='$day') location '/user/hadoop/odin/ac/$day/'; "
+    alter table ad_click add partition(day='$day') location '/user/hadoop/odin/ac/$day/';
     alter table ad_fe_imp add partition(day='$day') location '/user/hadoop/odin/ad_feimp/$day/'; "
 
 
